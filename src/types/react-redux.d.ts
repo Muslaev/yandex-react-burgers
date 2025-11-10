bf4d40c49e0f677a2472e44b57cdd4314f31bc16
Muslaev <MumbayOSK@yandex.ru>
2025-11-10 11:57:12 +0300

    Updates

--- src/types/react-redux.d.ts
+++
@@ -1,7 +0,0 @@
-import { store } from '@store/index';
-import { useDispatch } from 'react-redux';
-
-declare module 'react-redux' {
-  interface DefaultRootState extends ReturnType<typeof store.getState> {}
-  export function useDispatch<TDispatch = typeof store.dispatch>(): TDispatch;
-}
